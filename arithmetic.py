f83d126a1e0f71ecf180abd2d7fa628b2aaa2be7
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 12:02:25 -0800

    attempt to fix decimals; not successful

--- arithmetic.py
+++ arithmetic.py
@@ -1,3 +1,6 @@
+from decimal import *
+getcontext().prec = 3
+
 def add(num1, num2):
     return num1 + num2
 
@@ -8,7 +11,8 @@ def multiply(num1, num2):
     return num1 * num2
 
 def divide(num1, num2):
-    return float(num1)/float(num2)
+    divide = num1/float(num2)
+    return Decimal(divide)
 
 def square(num1):
     return num1 * num1
